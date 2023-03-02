a0097f5147ec9cca0eb8d18d930c8bb1b2399d8a
Cornel <cornel.paris@gmail.com>
2023-03-02 15:20:19 +0100

    adding the files

---
+++ Code.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main ()
+
+
+{
+
+printf("This is the Git checkpoint task 3.1");
+
+return 0;
+
+}
---
+++ code2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+
+{
+
+printf("this is the  Git checkpoint3 task");
+
+return 0;
+
+}
+
